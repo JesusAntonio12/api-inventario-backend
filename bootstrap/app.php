ecad3432a596d4a69f3c0c6bfc9de5c05fd29c23
JesusAntonio12 <jesuscastyillocoronado@gmail.com>
2026-08-05 19:09:21 -0600

    Update

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,6 +14,8 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware): void {
         //
     })
-    ->withExceptions(function (Exceptions $exceptions): void {
-        //
+    ->withExceptions(function (Exceptions $exceptions) {
+        $exceptions->render(function (Illuminate\Auth\AuthenticationException $e, $request) {
+            return response()->json(['message' => 'No autenticado'], 401);
+        });
     })->create();
